6bd6df4fae977dc1149a1e440a17fabca67d4285
Leonid_Borisevich <leonid_borisevich@epam.com>
2019-03-03 14:19:48 +0300

    Initial commit

---
+++ src/main/java/zhi/yest/jibexample/JibExampleApplication.java
@@ -0,0 +1,12 @@
+package zhi.yest.jibexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JibExampleApplication.class, args);
+    }
+}
