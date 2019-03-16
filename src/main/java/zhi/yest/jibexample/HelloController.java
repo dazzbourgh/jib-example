9ff17856c11d49b197071f1f778f45760dd91f73
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-03-16 19:01:27 +0300

    customized caption

--- src/main/java/zhi/yest/jibexample/HelloController.java
+++ src/main/java/zhi/yest/jibexample/HelloController.java
@@ -1,6 +1,7 @@
 package zhi.yest.jibexample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping({"/hello", ""})
 public class HelloController {
     @GetMapping
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("caption", "hello");
         return "hello";
     }
 }
--- src/main/java/zhi/yest/jibexample/JibExampleApplication.java
+++ src/main/java/zhi/yest/jibexample/JibExampleApplication.java
@@ -2,18 +2,11 @@ package zhi.yest.jibexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class JibExampleApplication implements WebMvcConfigurer {
+public class JibExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JibExampleApplication.class, args);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
