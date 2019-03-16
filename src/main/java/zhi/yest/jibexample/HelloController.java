40fdb67877fa01970c899ac3f629fbea8e2b9c49
Leonid Borisevich <leonidborisevich00@gmail.com>
2019-03-16 18:40:14 +0300

    beautified hello page

--- src/main/java/zhi/yest/jibexample/HelloController.java
+++ src/main/java/zhi/yest/jibexample/HelloController.java
@@ -1,14 +1,15 @@
 package zhi.yest.jibexample;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping({"/hello", ""})
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "hello pull request";
+        return "hello";
     }
 }
