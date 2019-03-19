57157c2ee215623ed6c060482de828de6454e5e0
Leonid_Borisevich <leonid_borisevich@epam.com>
2019-03-19 13:37:53 +0300

    changed caption

--- src/main/java/zhi/yest/jibexample/HelloController.java
+++ src/main/java/zhi/yest/jibexample/HelloController.java
@@ -1,14 +1,16 @@
 package zhi.yest.jibexample;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping({"/hello", ""})
 public class HelloController {
     @GetMapping
-    public String hello() {
-        return "hello, pull request";
+    public String hello(Model model) {
+        model.addAttribute("caption", "hello pull request");
+        return "hello";
     }
 }
